479926ece32411c00ae7ef1a4ab180aa97eacf0d
alex9238 <alexlorey92@gmail.com>
2025-11-14 11:51:44 -0600

    Initial Commit

---
+++ assitantRetailStarbucks/assitantRetailStarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  assitantRetailStarbucksApp.swift
+//  assitantRetailStarbucks
+//
+//  Created by Lidiana Parada on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct assitantRetailStarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
